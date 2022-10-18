0fa016ea678d947f7aa96aa3ee65d6e7eeb2f798
unknown <gabriel10.speed@hotmail.com>
2022-10-18 20:46:08 -0300

    Refactoring

--- refactor.js
+++ refactor.js
@@ -3,25 +3,27 @@ function returnRandomNumber() {
 }
 
 function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
+  return `Seu número é ${Math.round(returnRandomNumber() * 100) + num}!`;
+}
 
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+function sumToRandomNumber(num) {
+  return `Seu número é ${Math.round(returnRandomNumber() * 100) + num}!`;
 }
 
 function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
+  return `Seu número é ${Math.round(returnRandomNumber() * 100) - num}!`;
 }
 
 function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
+  return `Seu número é ${Math.round(returnRandomNumber() * 100) * num}!`;
 }
 
 function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+  return `Seu número é ${Math.round(returnRandomNumber() * 100) / num}!`;
 }
+
+console.log(returnRandomNumber())
+console.log(sumToRandomNumber(20))
+console.log(subtractRandomNumber(20))
+console.log(multiplyToRandomNumber(20))
+console.log(divideRandomNumber(20))
